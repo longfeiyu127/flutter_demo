83224e0e242c6dca934f6a50ee55eb1576e78634
龙先喜 <ex-longxx001@cmrh.com>
2019-03-14 21:11:56 +0800

    list demo

---
+++ lib/model/post.dart
@@ -0,0 +1,30 @@
+class Post {
+  const Post({
+    this.title,
+    this.author,
+    this.imageUrl
+  });
+
+  final String title;
+  final String author;
+  final String imageUrl;
+
+}
+
+final List<Post> posts = [
+  Post(
+    title: "1",
+    author: "zhangsan",
+    imageUrl: "https://www-di1.dev.cmrh.com/cms/static/img/09fcfafc2131763925b4bd113942a4ee.png"
+  ),
+  Post(
+      title: "2",
+      author: "zhangsan",
+      imageUrl: "https://www-di1.dev.cmrh.com/cms/static/img/09fcfafc2131763925b4bd113942a4ee.png"
+  ),
+  Post(
+      title: "3",
+      author: "zhangsan",
+      imageUrl: "https://www-di1.dev.cmrh.com/cms/static/img/09fcfafc2131763925b4bd113942a4ee.png"
+  ),
+];
